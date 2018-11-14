523eb52a2363e64171fe36d1f8c0754b394bd197
Colin <bightm80144@gmail.com>
2018-11-14 21:08:59 +0800

    optimize gif file

--- src/container.js
+++ src/container.js
@@ -64,107 +64,120 @@ export default class Container extends React.Component {
 						<ReactCSSTransitionGroup
 					      transitionName="text1"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={4000}>		
+					      transitionAppearTimeout={4000}
+					      transitionLeaveTimeout={4000}>		
 							<li key='1'>A</li>
 						</ReactCSSTransitionGroup>		
 							<li key='2'>  </li>
 						<ReactCSSTransitionGroup
 					      transitionName="text2"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={3700}>		
+					      transitionAppearTimeout={3700}
+					      transitionLeaveTimeout={3700}>		
 							<li key='3'>W</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text3"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={3400}>		
+					      transitionAppearTimeout={3400}
+					      transitionLeaveTimeout={3400}>		
 							<li key='4'>e</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text4"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={3100}>		
+					      transitionAppearTimeout={3100}
+					      transitionLeaveTimeout={3100}>		
 							<li key='5'>b</li>
 						</ReactCSSTransitionGroup>
 							<li>  </li>
 						<ReactCSSTransitionGroup
 					      transitionName="text5"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={2800}>		
+					      transitionAppearTimeout={2800}
+					      transitionLeaveTimeout={2800}>		
 							<li key='6'>D</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text6"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={2500}>		
+					      transitionAppearTimeout={2500}
+					      transitionLeaveTimeout={2500}>		
 							<li key='7'>e</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text7"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={2200}>		
+					      transitionAppearTimeout={2200}
+					      transitionLeaveTimeout={2200}>		
 							<li key='8'>v</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text8"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={1900}>		
+					      transitionAppearTimeout={1900}
+					      transitionLeaveTimeout={1900}>		
 							<li key='9'>e</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text9"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={1600}>		
+					      transitionAppearTimeout={1600}
+					      transitionLeaveTimeout={1600}>		
 							<li key='10'>l</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text10"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={1300}>		
+					      transitionAppearTimeout={1300}
+					      transitionLeaveTimeout={1300}>		
 							<li key='11'>o</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text11"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={1000}>		
+					      transitionAppearTimeout={1000}
+					      transitionLeaveTimeout={1000}>		
 							<li key='12'>p</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text12"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={700}>		
+					      transitionAppearTimeout={700}
+					      transitionLeaveTimeout={700}>		
 							<li key='13'>e</li>
 						</ReactCSSTransitionGroup>
 						<ReactCSSTransitionGroup
 					      transitionName="text13"
 					      transitionEnter={false}
-					      transitionLeave={false}
+					      transitionLeave={true}
 					      transitionAppear={true}
-					      transitionAppearTimeout={400}>		
+					      transitionAppearTimeout={400}
+					      transitionLeaveTimeout={400}>		
 							<li key='14'>r</li>
 						</ReactCSSTransitionGroup>					
 					</ul>
